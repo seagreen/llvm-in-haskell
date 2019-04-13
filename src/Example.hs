f2804be2cee3a2b17e00a51385fa982c6ea1c1b2
Ian Grant Jeffries <ian@housejeffries.com>
2019-04-13 15:04:23 -0400

    Add 'constant' helper function.

--- src/Example.hs
+++ src/Example.hs
@@ -9,8 +9,8 @@ import LLVM.IRBuilder.Monad (IRBuilderT, block, named)
 import Prelude
 
 import qualified LLVM.AST as AST
+import qualified LLVM.AST.Constant as C
 import qualified LLVM.AST.IntegerPredicate as IntegerPredicate
-import qualified LLVM.IRBuilder.Constant as IRBuilder.Constant
 
 helloWorld :: AST.Module
 helloWorld =
@@ -28,7 +28,7 @@ helloWorld =
       hello <- globalStringPtr "hello world" "hello"
       _ <- call printf [(fmt, mempty), (hello, mempty)]
 
-      ret =<< IRBuilder.Constant.int64 0
+      ret (constant 0) -- For 'constant' see the helper function below
 
 functions :: AST.Module
 functions =
@@ -42,18 +42,16 @@ functions =
                   ret =<< add n n
 
       plusOne <- function "plus_one" [(i64, "n")] i64 $ \[n] -> do
-                   one <- IRBuilder.Constant.int64 1
-                   ret =<< add n one
+                   ret =<< add n (constant 1)
 
       _ <- function "main" mempty i64 $ \_ -> do
-             two <- IRBuilder.Constant.int64 2
-             res1 <- call double [(two, mempty)]
+             res1 <- call double [(constant 2, mempty)]
              res2 <- call plusOne [(res1, mempty)]
 
              fmt <- globalStringPtr "%d\n" "fmt"
              _ <- call printf [(fmt, mempty), (res2, mempty)]
 
-             ret =<< IRBuilder.Constant.int64 0
+             ret (constant 0)
 
       pure ()
 
@@ -65,27 +63,28 @@ branching =
     f = do
       printf <- externVarArgs "printf" [ptr i8] i64
 
-      one <- IRBuilder.Constant.int64 1
-      two <- IRBuilder.Constant.int64 2
-      three <- IRBuilder.Constant.int64 3
-
       _ <- function "main" mempty i64 $ \_ -> mdo
              _ <- named block "Entry"
-             test <- icmp IntegerPredicate.EQ one two
+             test <- icmp IntegerPredicate.EQ (constant 1) (constant 2)
              condBr test ifEqual ifUnequal
 
              ifEqual <- named block "IfEqual"
-             res1 <- add two two
+             res1 <- add (constant 2) (constant 2)
              br end
 
              ifUnequal <- named block "IfUnequal"
-             res2 <- add three three
+             res2 <- add (constant 3) (constant 3)
              br end
 
              end <- named block "End"
              res3 <- phi [(res1, ifEqual), (res2, ifUnequal)]
              fmt <- globalStringPtr "%d\n" "fmt"
              _ <- call printf [(fmt, mempty), (res3, mempty)]
-             ret =<< IRBuilder.Constant.int64 0
+             ret (constant 0)
 
       pure ()
+
+-- | Helper function.
+constant :: Integer -> AST.Operand
+constant n =
+  AST.ConstantOperand (C.Int 64 n)
